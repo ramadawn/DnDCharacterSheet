caaa681cb083e342823425322fcf9c841122bc07
Squeakyclarinet <62899621+Squeakyclarinet@users.noreply.github.com>
2020-11-12 13:21:23 -0800

    Update and rename battle.java to Battle.java

--- java/com/info3245/gamemasterutility/Battle.java
+++ java/com/info3245/gamemasterutility/Battle.java
@@ -7,13 +7,18 @@ public class Battle {
     private int newHealthValue = Integer.parseInt("0");
 
     public int damageTaken( String aWeapon, String aHealth) {
+        
+        //Get attack and health
         attackValue = Integer.parseInt(aWeapon);
         ogHealthValue = Integer.parseInt(aHealth);
 
+        //Calculate attack die roll
         int attackDie = new Random().nextInt(attackValue+1);
 
+        //Calculate remaining health
         newHealthValue = ogHealthValue - attackDie;
 
+        //Return new health
         return this.newHealthValue;
 
     }
