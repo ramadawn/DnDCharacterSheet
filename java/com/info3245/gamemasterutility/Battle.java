7b67cb1d2d99ba0c878e05ef13b8001405651d60
Squeakyclarinet <62899621+Squeakyclarinet@users.noreply.github.com>
2020-11-12 15:58:48 -0800

    Update Battle.java

--- java/com/info3245/gamemasterutility/Battle.java
+++ java/com/info3245/gamemasterutility/Battle.java
@@ -2,25 +2,35 @@ import java.util.Random;
 
 public class Battle {
 
-    private int attackValue = Integer.parseInt("0");
-    private int ogHealthValue = Integer.parseInt("0");
-    private int newHealthValue = Integer.parseInt("0");
-
-    public int damageTaken( String aWeapon, String aHealth) {
-        
-        //Get attack and health
-        attackValue = Integer.parseInt(aWeapon);
-        ogHealthValue = Integer.parseInt(aHealth);
+    public int damageTaken(String aWeapon, int aHealth) {
 
         //Calculate attack die roll
-        int attackDie = new Random().nextInt(attackValue+1);
-
-        //Calculate remaining health
-        newHealthValue = ogHealthValue - attackDie;
+        int attackDie = getAttackDie(aWeapon);
+        int attackRoll = new Random().nextInt(attackDie+1);
 
         //Return new health
-        return this.newHealthValue;
+        return aHealth - attackRoll;
 
+    }
+    
+    //Method for finding weapon attack die
+    public int getAttackDie(String aName) {
+        switch(aName){
+            case "Sword":
+                return 8;
+            case "Dagger":
+                return 4;
+            case "Bow":
+                return 6;
+            case "Halberd":
+                return 10;
+            case "Lance":
+                return 10;
+            case "Axe":
+                return 6;
+            default:
+                throw new IllegalStateException("Unexpected value: " + aName);
+        }
     }
 
 }
