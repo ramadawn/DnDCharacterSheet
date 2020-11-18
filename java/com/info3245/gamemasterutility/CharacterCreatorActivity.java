ec66f0810f1af64d51391fe4f196c7400a927f8b
Zurhydryh <zurhydryh@gmail.com>
2020-11-17 19:44:23 -0800

    Update: Save & CharacterCreatorActivity
    Update for testing purposes. Added toasts to show if file saved successfully.

--- java/com/info3245/gamemasterutility/CharacterCreatorActivity.java
+++ java/com/info3245/gamemasterutility/CharacterCreatorActivity.java
@@ -3,6 +3,7 @@ package com.info3245.gamemasterutility;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class CharacterCreatorActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class CharacterCreatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_creator);
+
+        CharacterSheet newCharacter = new CharacterSheet();
+        newCharacter.setAttributes("8","3","6","2","2","7");
+        newCharacter.setSkills("4","2","7","2","7","3",
+                "1","8","0","3","1","4","1","7","3","4","4","2");
+        newCharacter.setWeapon1("sword");
+        newCharacter.setName("Ronaldo");
+        newCharacter.setAC("6");
+        newCharacter.setMaxHP("30");
+        newCharacter.fullHealth();
+        newCharacter.setSaveFileName(newCharacter.getName() + "_save.txt");
+        Save.saveSheet(newCharacter, this);
+
     }
 }
--- java/com/info3245/gamemasterutility/Save.java
+++ java/com/info3245/gamemasterutility/Save.java
@@ -1,9 +1,14 @@
+package com.info3245.gamemasterutility;
+
+import android.content.Context;
+import android.widget.Toast;
+
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
 
 
 public class Save {
-		public static void saveSheet(CharacterSheet character)  {
+		public static void saveSheet(CharacterSheet character, Context context)  {
 			
 			String output = character.toString();
 			String path = character.getSaveFileName();
@@ -13,17 +18,12 @@ public class Save {
 			      FileWriter myWriter = new FileWriter(path);
 			      myWriter.write(output);
 			      myWriter.close();
-			      System.out.println("Successfully saved character sheet");
+			      Toast.makeText(context,"Save SUCCESS",Toast.LENGTH_LONG).show();
 			    } catch (IOException e) {
-			      System.out.println("An error occurred.");
-			      e.printStackTrace();
+					Toast.makeText(context,"Save FAILED",Toast.LENGTH_LONG).show();
+					e.printStackTrace();
 			    }
-			
-
-			
 			}
-		    
-		  
 		}	
 
 
